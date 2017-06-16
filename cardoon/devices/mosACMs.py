Fixed mistake in ACMs and overflow problem in f_simple (EKV)

cardoon/devices/mosACMs.py
@@ -74,8 +74,8 @@ class Device(cir.Element):
         w = ('Channel width', 'm', float, 10e-6),
         l = ('Channel length', 'm', float, 10e-6),
         vth = ('Threshold Voltage', 'V', float, 0.5),
-        isq = ('Sheet normalization current', 'A/V^2', float, 100e-9),
-        n = ('Subthreshold slope factor', 'F/m^2', float, 1.3),
+        isq = ('Sheet normalization current', 'A', float, 100e-9),
+        n = ('Subthreshold slope factor', '', float, 1.3),
         cox = ('Gate oxide capacitance per area', 'F/m^2', float, 0.7e-3),
         tcv = ('Threshold voltage temperature coefficient', 'V/K', 
                float, 0.001),
@@ -171,7 +171,7 @@ class Device(cir.Element):
         i_f = inv_f((vp - vPort1[2]) / self._Vt)
         i_r = inv_f((vp - vPort1[0]) / self._Vt)
         
-        idrain = self._IS * (i_f - i_r) 
+        idrain = self._tf * self._IS * (i_f - i_r) 
 
         iVec, qVec = np.array([idrain]), np.array([])
 

cardoon/devices/mosEKV.py
@@ -22,15 +22,18 @@ def f_simple(v):
 
     Not accurate for moderate inversion
     """
-    # Have to treat the function for large negative v specially
-    # otherwise exp(.5*v) << 1 and we get log(1) = 0
-    b = v + 20.
     d = np.exp(.5 * v)
     c = np.log(1. + d)
-    # f = (b > -20) ? c : d
-    f = ad.condassign(b, c, d)
+    # Needs special treatment for large negative v 
+    # otherwise exp(.5*v) << 1 and we get log(1) = 0
+    b1 = v + 20.
+    # f = (v > -20) ? c : d
+    f = ad.condassign(b1, c, d)
+    # Also needs special treatment for large v to prevent overflow
+    b2 = v - 35.
+    # f = (v > 35) ? .5*v : f
+    f = ad.condassign(b2, .5*v, f)
     return f**2
-
 
 def f_accurate(v):
     """